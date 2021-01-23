1c768d437aeb83d01601fd2c53a00d5db54855a0
jlelito <joshualelito@gmail.com>
2021-01-23 15:49:55 -0500

    Adding event alerts to frontend

--- src/App.js
+++ src/App.js
@@ -109,6 +109,14 @@ class App extends Component {
       this.state.goFundContract.methods.createCampaign(seconds, _name, _goal).send({ from: this.state.account }).on('transactionHash', (hash) => {
         this.setState({hash:hash})
         this.setState({action: 'Created Campaign'})
+        this.state.goFundContract.events.campaignCreated({}, async (error, event) => {
+          let campID = event.returnValues.id
+          let name = event.returnValues.name
+          let duration = event.returnValues.duration
+          let goal = event.returnValues.goal
+          window.alert('Created Campaign! \n\n' + 'Campaign ID: ' + campID + '\nName: ' + name +  '\nDuration: ' + duration + '\nGoal: ' + window.web3.utils.fromWei(goal, 'Ether'))
+          await this.updateCampaigns()
+      })
         
       })
       } catch(e) {
@@ -123,6 +131,12 @@ class App extends Component {
       this.state.goFundContract.methods.fundCampaign(campId).send({ from: this.state.account, value: amount }).on('transactionHash', (hash) => {
         this.setState({hash:hash})
         this.setState({action: 'Contributed'})
+        this.state.goFundContract.events.campaignFunded({}, async (error, event) => {
+          let campID = event.returnValues.id
+          let amount = event.returnValues.amount
+          window.alert('Funded Campaign! \n\n' + 'Campaign ID: ' + campID + '\nAmount: ' + window.web3.utils.fromWei(amount, 'Ether') + ' ETH')
+          await this.updateCampaigns()
+      })
         
       })
     } catch(e) {
@@ -135,6 +149,12 @@ class App extends Component {
       this.state.goFundContract.methods.withdraw(campId).send({ from: this.state.account }).on('transactionHash', (hash) => {
         this.setState({hash:hash})
         this.setState({action: 'Withdrawed'})
+        this.state.goFundContract.events.campaignWithdrawed({}, async (error, event) => {
+          let campID = event.returnValues.id
+          let amount = event.returnValues.amount
+          window.alert('Withdrawed from Campaign! \n\n' + 'Campaign ID: ' + campID + '\nAmount: ' + window.web3.utils.fromWei(amount, 'Ether') + ' ETH')
+          await this.updateCampaigns()
+      })
        
       })
     } catch(e) {
@@ -147,6 +167,12 @@ class App extends Component {
       this.state.goFundContract.methods.refund(campId).send({ from: this.state.account }).on('transactionHash', (hash) => {
         this.setState({hash:hash})
         this.setState({action: 'Refunded Contribution'})
+        this.state.goFundContract.events.campaignRefunded({}, async (error, event) => {
+          let campID = event.returnValues.id
+          let amount = event.returnValues.amount
+          window.alert('Received refund from Campaign! \n\n' + 'Campaign ID: ' + campID + '\nAmount: ' + window.web3.utils.fromWei(amount, 'Ether') + ' ETH')
+          await this.updateCampaigns()
+      })
        
       })
       } catch(e) {
