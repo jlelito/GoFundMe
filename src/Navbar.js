6afaad672dbe503605418a1d20fce0ba6f84926a
jlelito <joshualelito@gmail.com>
2021-01-23 11:29:58 -0500

    changing double quotes to single quotes

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,21 +7,21 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
         <div
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          className='navbar-brand col-sm-3 col-md-2 mr-0'
           
         >
-          <img src={pic} width="30" height="30" className="d-inline-block align-top img-circle mr-2" alt="" />
+          <img src={pic} width='30' height='30' className='d-inline-block align-top img-circle mr-2' alt='' />
           
             GoFundMe
           
         </div>
         
-        <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-secondary">
-              <small id="account" className="text-white">{this.props.account}</small>
+        <ul className='navbar-nav px-3'>
+          <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+            <small className='text-secondary'>
+              <small id='account' className='text-white'>{this.props.account}</small>
             </small>
             { this.props.account
               ? <img
