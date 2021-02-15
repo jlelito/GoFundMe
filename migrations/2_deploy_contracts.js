5961be3baf688c3a542ff02d4eb00ccfda272b7e
jlelito <joshualelito@gmail.com>
2021-02-14 19:46:06 -0500

    Removing contract const from deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,4 @@ const GoFundMe = artifacts.require("GoFundMe");
 
 module.exports = async function(deployer, accounts) {
   deployer.deploy(GoFundMe);
-  const contract = await GoFundMe.deployed();
 };
